7d18f2b37a11df6a91d7156315640b89d38557a1
Max Cascone <max@bitovi.com>
2024-10-02 13:50:31 -0500

    back to PAT

--- approve.sh
+++ approve.sh
@@ -11,11 +11,21 @@
 
 
 
+# curl -L \
+#   -X POST \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer $GH_TOKEN" \
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/repos/$REPO/actions/runs/$RUNID/pending_deployments \
+#   -d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
+#   --verbose
+
+
 curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GH_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$REPO/actions/runs/$RUNID/pending_deployments \
-  -d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
-  --verbose
+-X POST \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: token $GH_TOKEN" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+https://api.github.com/repos/$REPO/actions/runs/11149406260/pending_deployments \
+-d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
+--verbose
