d6812c75a5ba4712cdc2c64f0e20acb7708ecbd9
Max Cascone <max@bitovi.com>
2024-10-02 15:37:09 -0500

    use 3rd party action to approve

--- approve.sh
+++ approve.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# gh api repos/mcascone/condition-testing/actions/runs/11148670910/pending_deployments  \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "X-GitHub-Api-Version: 2022-11-28"    \
+gh api repos/mcascone/condition-testing/actions/runs/11148670910/pending_deployments  \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28"    \
 #   --method POST                                       \
 #   -F "environment_ids[]=11130481765"                    \
 #   -f "state=approved"                                 \
@@ -21,11 +21,11 @@
 #   --verbose
 
 
-curl -L \
--X POST \
--H "Accept: application/vnd.github.v3+json" \
--H "Authorization: token $GH_TOKEN" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/$REPO/actions/runs/11149406260/pending_deployments \
--d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
---verbose
+# curl -L \
+# -X POST \
+# -H "Accept: application/vnd.github.v3+json" \
+# -H "Authorization: token $GH_TOKEN" \
+# -H "X-GitHub-Api-Version: 2022-11-28" \
+# https://api.github.com/repos/$REPO/actions/runs/11149406260/pending_deployments \
+# -d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
+# --verbose
