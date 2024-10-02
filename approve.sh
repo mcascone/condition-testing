ccd11cfc59c8b6cb9fd7002987df269aa08a5ace
Max Cascone <max@bitovi.com>
2024-10-02 13:13:04 -0500

    checkout

--- approve.sh
+++ approve.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-gh api repos/$REPO/actions/runs/$RUNID/pending_deployments  \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28"    \
-  --method POST                                       \
-  -F environment_ids[]=$ENVID                     \
-  -f state="approved"                                 \
-  -f comment="Auto-Approved by GitHub Actions"      \
---verbose
-
-
-
-# curl -Lv \
+# gh api repos/mcascone/condition-testing/actions/runs/11148670910/pending_deployments  \
 #   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer $GH_TOKEN" \
-#   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   https://api.github.com/repos/$REPO/actions/runs/$RUNID/pending_deployments \
-#   -d '{"environment_ids":[161171787],"state":"approved","comment":"Ship it!"}'
+#   -H "X-GitHub-Api-Version: 2022-11-28"    \
+#   --method POST                                       \
+#   -F "environment_ids[]=11130481765"                    \
+#   -f "state=approved"                                 \
+#   -f "comment=Auto-Approved by GitHub Actions"      \
+# --verbose
 
 
-  # -H "X-GitHub-Api-Version: 2022-11-28"             \
-  # -H "Accept: application/vnd.github+json"     \
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/actions/runs/$RUNID/pending_deployments \
+  -d '{"environment_ids":[11130481765],"state":"approved","comment":"Ship it!"}' \
+  --verbose
